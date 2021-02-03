b7bb7426917f99276b60d103dbd56003b1efad7f
toricor <toriyabe0821@gmail.com>
2021-02-03 23:30:45 +0900

    impl last

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -338,6 +338,9 @@ func TestBuiltinFunctions(t *testing.T) {
 		{`first([1, 2, 3])`, 1},
 		{`first([])`, nil},
 		{`first(1)`, "argument to `first` must be ARRAY, got INTEGER"},
+		{`last([1, 2, 3])`, 3},
+		{`last([])`, nil},
+		{`last(1)`, "argument to `last` must be ARRAY, got INTEGER"},
 	}
 
 	for _, tt := range tests {
