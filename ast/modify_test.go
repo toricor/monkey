420b1edda0b2abdba1fda73e177c9780bdd76243
toricor <toriyabe0821@gmail.com>
2021-02-16 23:30:10 +0900

    impl case for LetStatement

--- ast/modify_test.go
+++ ast/modify_test.go
@@ -91,6 +91,10 @@ func TestModify(t *testing.T) {
 			&ReturnStatement{ReturnValue: one()},
 			&ReturnStatement{ReturnValue: two()},
 		},
+		{
+			&LetStatement{Value: one()},
+			&LetStatement{Value: two()},
+		},
 	}
 
 	for _, tt := range tests {
