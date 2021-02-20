1d1cf71ecdbcc4bbdb1cd188522e916d289d8471
toricor <toriyabe0821@gmail.com>
2021-02-20 22:06:12 +0900

    can parse macro literal

--- ast/ast.go
+++ ast/ast.go
@@ -336,3 +336,28 @@ func (hl *HashLiteral) String() string {
 
 	return out.String()
 }
+
+type MacroLiteral struct {
+	Token      token.Token // 'macro' トークン
+	Parameters []*Identifier
+	Body       *BlockStatement
+}
+
+func (fl *MacroLiteral) expressionNode()      {}
+func (fl *MacroLiteral) TokenLiteral() string { return fl.Token.Literal }
+func (fl *MacroLiteral) String() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range fl.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString(fl.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") ")
+	out.WriteString(fl.Body.String())
+
+	return out.String()
+}
